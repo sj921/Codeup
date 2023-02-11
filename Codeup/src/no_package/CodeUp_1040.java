c1162ebb50b237ed11ad755ca63a4ae56bba10f0
sj_92 <sj_92@DESKTOP-2822HVG>
2023-02-11 23:57:42 +0900

    .

---
+++ Codeup/src/no_package/CodeUp_1040.java
@@ -0,0 +1,14 @@
+package no_package;
+
+import java.util.Scanner;
+
+public class CodeUp_1040 {
+
+	public static void main(String[] args) {
+		System.out.print(">> ");
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		System.out.printf("%d", -1 * num);
+		
+	}
+}
---
+++ Codeup/src/no_package/CodeUp_1041.java
@@ -0,0 +1,13 @@
+package no_package;
+
+import java.util.Scanner;
+
+public class CodeUp_1041 {
+	
+	public static void main(String[] args) {
+		System.out.print(">> ");
+		Scanner sc = new Scanner(System.in);		
+		char c = sc.next().charAt(0);
+		System.out.printf("%c", c+1);		
+	}
+}
